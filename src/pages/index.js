f6d40cbe28527d0d4a2d40ec7618b0375cc02599
Lucio Canepa <luciocanepa@macbook-pro-di-lucio.home>
2021-08-25 13:32:20 +0200

    Aggiunto il favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,33 @@
 import * as React from "react"
+import {Helmet} from 'react-helmet'
+
 import avatar from "../images/avatar.png"
 import pc from "../images/pc.svg"
 import uni from "../images/uni.svg"
 import sport from "../images/sport.svg"
+import favicon from "../images/favicon.png"
 
 import "../components/style.css"
 
 const indexPage = () => {
   return(
-    <div className="welcome">
-      <div className="welcome-photo">
-        <img src={avatar}></img>
+      <div className="welcome">
+      <Helmet>
+        <title>luciocanepa</title>
+        <link rel="icon" href={favicon}/>
+      </Helmet>
+        <div className="welcome-photo">
+          <img src={avatar}></img>
+        </div>
+        <div className="welcome-text">
+          <h1>Lucio Canepa</h1>
+          <ul>
+            <li><img src={uni}/> Matematica presso ETH Zürich </li>
+            <li><img src={pc}/> Appassionato di design e informatica </li>
+            <li><img src={sport}/> Sport: corsa e arbitro di calcio </li>
+          </ul>
+        </div>
       </div>
-      <div className="welcome-text">
-        <h1>Lucio Canepa</h1>
-        <ul>
-          <li><img src={uni}/> Matematica presso ETH Zürich </li>
-          <li><img src={pc}/> Appassionato di design e informatica </li>
-          <li><img src={sport}/> Sport: corsa e arbitro di calcio </li>
-        </ul>
-      </div>
-    </div>
   )
 }
 
